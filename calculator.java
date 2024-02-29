e6a36c9e6165e9f3b6d2616e5159a5d989bab156
BishantRajbanshi <rajbancbishant@gmail.com>
2024-02-29 17:03:20 +0545

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,37 +1,42 @@
 import java.util.Scanner;
 
-public class calculator {
+public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("What do you want to perform: ");
-        String ope = scanner.nextLine();
+        String operation = scanner.nextLine();
 
         System.out.print("Enter the first number:");
         double num1 = scanner.nextDouble();
 
-        System.out.println("Enter the second number:");
+        System.out.print("Enter the second number:");
         double num2 = scanner.nextDouble();
+        
+        double result = 0.0;
 
-        if (ope.equals("add")) {
-            double result = num1 + num2;
-            System.out.println("Result: " + result);
-        } else if (ope.equals("sub")) {
-          double result = num1 - num2;
-          System.out.println("Result: " + result);
-        } else if (ope.equals("mul")) {
-          double result = num1 * num2;
-          System.out.println("Result: " + result);
-        } else if (ope.equals("div")) {
-          double result = num1 / num2;
-          System.out.println("Result: " + result);
-        } else if (ope.equals("mod")) {
-          double result = num1 % num2;
-          System.out.println("Result: " + result);
+        switch (operation) {
+            case "add":
+                result = num1 + num2;
+                break;
+            case "sub":
+                result = num1 - num2;
+                break;
+            case "mul":
+                result = num1 * num2;
+                break;
+            case "div":
+                result = num1 / num2;
+                break;
+            case "mod":
+                result = num1 % num2;
+                break;
+            default:
+                System.out.println("Invalid operation.");
+                break;
         }
-        else {
-            System.out.println("Unsupported operation.");
-        }
+
+        System.out.println("Result: " + result);
         scanner.close();
     }
 }
