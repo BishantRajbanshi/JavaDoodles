3617a94133e76ea431dca0c87d17e56fa667ae58
BishantRajbanshi <rajbancbishant@gmail.com>
2024-03-01 12:20:40 +0545

    update

---
+++ w1/name.java
@@ -0,0 +1,15 @@
+package w1;
+
+public class name {
+  public static void main(String[] args) {
+    String name = "Bishant";
+    int age = 4;
+    String studentnumber = "54545454";
+
+    System.out.println("Name: "+ name);
+    System.out.println("Age: "+ age);
+    System.out.println("Student Number: "+ studentnumber);
+  }
+}
+
+
