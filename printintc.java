3201f8040cc61605591deba098f96bb97be879b0
BishantRajbanshi <rajbancbishant@gmail.com>
2024-03-01 12:20:30 +0545

    sss

---
+++ printintc.java
@@ -0,0 +1,14 @@
+public class printintc {
+  public static void main(String[] args) {
+      int a = 7;
+      double b = 7.77;
+      char c = 'c';
+
+      System.out.println("The int is: " + a);
+      System.out.println("The double is: " + b);
+      System.out.println("The character is: " + c);
+  }
+}
+
+
+
