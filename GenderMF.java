efcf1ef772eeb40d93564fd8d60b19cb83cda54b
BishantRajbanshi <rajbancbishant@gmail.com>
2024-03-05 14:03:23 +0545

    update

---
+++ GenderMF.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class GenderMF {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the gender(M/F): ");
+    char Gender = sc.next().charAt(0);
+
+    switch (Gender) {
+      case 'M':
+        System.out.println("The gender is male");
+        break;
+
+      case 'F':
+        System.out.println("The gender is female");
+        break;
+
+      default:
+        System.out.println("Invalid input");
+        break;
+    }
+    sc.close();
+  }
+}
---
+++ sumGreater.java
@@ -0,0 +1,14 @@
+public class sumGreater {
+  public static void main(String[] args) {
+    int number1 = 15;
+    int number2 = 20;
+
+    int sum = number1 + number2;
+
+    if (sum >= 20){
+      System.out.println("The sum is greater then 20");
+    }else{
+      System.out.println("The sum is smaller than 20");
+    }
+  }
+}
