bfdacda6107242be76edb79d278f8c0943d27576
BishantRajbanshi <rajbancbishant@gmail.com>
2024-03-06 22:42:37 +0545

    /.

--- w2/quitOf3.java
+++ w2/quitOf3.java
@@ -1,5 +1,12 @@
 public class quitOf3 {
   public static void main(String[] args) {
-    
+    int i = 1;
+    while (i < 10){
+      if (i == 3) {
+        break;
+      }
+      System.out.println(i);
+      i++;
+    }
   }
 }
