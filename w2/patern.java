5511a12514eab3d84963c9c4076832131a385507
BishantRajbanshi <rajbancbishant@gmail.com>
2024-03-06 12:37:53 +0545

    updtae

---
+++ w2/patern.java
@@ -0,0 +1,29 @@
+// Printing pattern using while loop
+
+// public class patern {
+//   public static void main(String[] args) {
+//     int i = 1;
+//     while (i <= 5){
+//       int j = 1;
+//       while (j <= i) {
+//         System.out.print("*");
+//         j++;
+//       }
+//       System.out.println();
+//       i++;
+//     }
+//   }
+// }
+
+// Printing pattern using for loop
+
+public class patern {
+  public static void main(String[] args){
+    for(int i=1; i<= 5 ; i++){
+      for(int j=1; j<=i ; j++ ){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
