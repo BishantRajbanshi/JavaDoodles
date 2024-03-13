88b7eccb3080e706e54749e4f04aa46a073e70fb
BishantRajbanshi <rajbancbishant@gmail.com>
2024-03-13 10:11:14 +0545

    upup

---
+++ Squarearea.java
@@ -0,0 +1,16 @@
+public class Square{
+  int length;
+  int breadth;
+
+   void getArea(int length,int breadth){
+   int Area = length * breadth;
+    
+  }
+}
+
+public class Squarearea {
+  public static void main(String[] args) {
+    Square square1 = new Square();
+
+  }
+}
