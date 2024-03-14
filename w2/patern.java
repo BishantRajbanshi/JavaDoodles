587fcb6ea894191d95978b6f4c4e1acfcfda62eb
BishantRajbanshi <rajbancbishant@gmail.com>
2024-03-14 22:33:20 +0545

    dqd

--- w2/patern.java
+++ w2/patern.java
@@ -17,13 +17,16 @@
 
 // Printing pattern using for loop
 
+
 public class patern {
-  public static void main(String[] args){
-    for(int i=1; i<= 5 ; i++){
-      for(int j=1; j<=i ; j++ ){
-        System.out.print("*");
+  public static void main(String[] args) {
+      for (int i = 1; i <= 5; i++) {  
+          for (int j = 1; j <= i; j++) { 
+              System.out.print("*");   
+          }
+          System.out.println();  
       }
-      System.out.println();
-    }
   }
 }
+
+
