c7ac4b989e94119e66176a1f62bd92b4378e4942
BishantRajbanshi <rajbancbishant@gmail.com>
2024-03-29 12:31:50 +0545

    dsfsf

---
+++ Workshop-5/Bus.java
@@ -0,0 +1,11 @@
+public class Bus extends Vehicle {
+  public void wheel() {
+      System.out.println("A bus typically has multiple wheels.");
+  }
+}
+
+
+
+
+
+
---
+++ Workshop-5/Cow.java
@@ -0,0 +1,17 @@
+public class Cow implements Animal, Printable {
+  public void eat() {
+      System.out.println("Cow is eating");
+  }
+
+  public void walk() {
+      System.out.println("Cow is walking");
+  }
+
+
+  public void display() {
+      System.out.println("Printing cow");
+  }
+}
+
+
+
