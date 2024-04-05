949799a35c5b048877563ee4187c68fe0cde3f60
BishantRajbanshi <rajbancbishant@gmail.com>
2024-04-05 11:13:02 +0545

    dfs

--- w6/tutorial.java
+++ w6/tutorial.java
@@ -1,22 +1,45 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
+// public class tutorial {
+//     public static void main(String[] args) {
+//         ArrayList<String> studentList = new ArrayList<>();
 
-public class tutorial {
+//         studentList.add("Nima");
+//         studentList.add("Emma");
+//         studentList.add("Pema");
+//         studentList.add("Lama");
+
+//         for (String student : studentList){
+//             System.out.println(student);
+//         }
+
+//         studentList.remove("Emma");
+//         System.out.println("After removal");
+//         for (String student : studentList){
+//             System.out.println(student);
+//         }
+//     }
+// }
+
+public class tutorial{
     public static void main(String[] args) {
         ArrayList<String> studentList = new ArrayList<>();
 
-        studentList.add("Nima");
-        studentList.add("Emma");
-        studentList.add("Pema");
-        studentList.add("Lama");
+        studentList.add("Hen");
+        studentList.add("Pen");
+        studentList.add("Sen");
+        studentList.add("Tenz");
 
-        for (String student : studentList){
+        System.out.println("Original list of students:");
+        for (String student : studentList) {
             System.out.println(student);
         }
 
-        studentList.remove("Emma");
-        System.out.println("After removal");
-        for (String student : studentList){
+        Collections.reverse(studentList);
+
+        System.out.println("\nReversed list of students:");
+        for (String student : studentList) {
             System.out.println(student);
         }
     }
