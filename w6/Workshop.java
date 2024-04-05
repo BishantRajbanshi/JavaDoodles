de274783baa7c06717f950f9b710a2fbb8409851
BishantRajbanshi <rajbancbishant@gmail.com>
2024-04-05 11:27:32 +0545

    hhh

---
+++ w6/Workshop.java
@@ -0,0 +1,35 @@
+// import java.util.ArrayList;
+
+// public class Workshop {
+
+//     public static void main(String[] args) {
+        
+//         ArrayList<String> studentsNames = new ArrayList<String>();
+
+//         studentsNames.add("Roman");
+//         studentsNames.add("Subash");
+//         studentsNames.add("Nima");
+//         studentsNames.add("Shema");
+//         studentsNames.add("Pima");
+
+//         System.out.println("Original List: "+ studentsNames);
+
+//         studentsNames.remove("Pima");
+
+//         System.out.println("After removal: " + studentsNames);
+//     }
+// }
+
+
+import java.util.ArrayList;
+
+public class Workshop {
+
+    public static void main(String[] args) {
+        
+        ArrayList<String> elements = new ArrayList<String>();
+
+        elements.add
+    }
+}
+
