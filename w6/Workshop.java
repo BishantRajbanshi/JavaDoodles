1d1907f4a8df4601427365983ff3c094165ce48c
BishantRajbanshi <rajbancbishant@gmail.com>
2024-04-05 11:32:42 +0545

    we

--- w6/Workshop.java
+++ w6/Workshop.java
@@ -21,15 +21,30 @@
 // }
 
 
+// import java.util.ArrayList;
+// import java.util.LinkedList;
+
+// public class Workshop {
+
+//     public static void main(String[] args) {
+        
+//         LinkedList<String> elements = new LinkedList<String>();
+
+//         elements.add("Red");
+//         elements.add("Blue");
+//         elements.add("Black");
+
+//         System.out.println("Original list: "+ elements);
+
+//         elements.addFirst("White");
+//         elements.addLast("Pink");
+
+//         System.out.println("After addition:" + elements);
+//       }
+// }
+
 import java.util.ArrayList;
 
-public class Workshop {
 
-    public static void main(String[] args) {
-        
-        ArrayList<String> elements = new ArrayList<String>();
 
-        elements.add
-    }
-}
 
