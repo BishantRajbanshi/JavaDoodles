752c3f70423cbf4ed13e07118fd4a2a22895f9e4
BishantRajbanshi <rajbancbishant@gmail.com>
2024-04-12 12:41:35 +0545

    hpp

--- Workshop-7/Test.java
+++ Workshop-7/Test.java
@@ -1,14 +1,14 @@
 public class Test {
   public static void main(String[] args) {
-      Customer customer1 = new Customer("John", "Doe");
-      Account account1 = new Account("John", "Doe", "123456789", 1000.0);
+      Customer customer1 = new Customer("Bill", "Gates");
+      Account account1 = new Account("Bill", "Gates", "5646546", 1000.0);
 
-      Customer customer2 = new Customer("Jane", "Smith");
-      Account account2 = new Account("Jane", "Smith", "987654321", 500.0);
+      Customer customer2 = new Customer("Marry", "Jane");
+      Account account2 = new Account("Marry", "Jane", "987654321", 500.0);
 
       Transaction.transfer(account1, account2, 200.0);
 
-      System.out.println("Account 1 Balance: " + account1.getBalance());
+      System.out.println("Account of"+ Customer.account1.firstName();  "Balance: " + account1.getBalance());
       System.out.println("Account 2 Balance: " + account2.getBalance());
   }
 }
